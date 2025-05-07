56b40007eac7593f1fc7c177b2a42d2c18c6c9e0
drishtant-lc <drishtant.chakraberty@lighthouse-canton.com>
2025-05-07 14:07:49 +0400

    Delete

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import EarningsDashboard from "./EarningsDashboard";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <EarningsDashboard />
-  </React.StrictMode>
-);
